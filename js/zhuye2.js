52a83ffd3d09509b4329da88b9be84cddff7aaff
chenbalabala <270210539@qq.com>
2019-12-26 19:26:44 +0800

    333

--- js/zhuye2.js
+++ js/zhuye2.js
@@ -1,4 +1,4 @@
- $(".c2_line1_1").hover(
+$(".c2_line1_1").hover(
     function () {
         var index = $(this).index();
         $('.c2_line1_1y').eq(index).show();
@@ -78,8 +78,8 @@ var swiper = new Swiper('.swiper-container', {
 slidesPerView: 3,
 spaceBetween: 30,
 slidesPerGroup: 3,
-loop: true,
-loopFillGroupWithBlank: true,
+loop: false,
+// loopFillGroupWithBlank: true,
 pagination: {
     el: '.swiper-pagination',
     clickable: true,
@@ -88,8 +88,7 @@ navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
 },
-});
- 
+}); 
 //说明隐藏与出现
 $(".c2_game1").on("mouseenter", function(){ //未来元素无效         
     $('.c2_game1_y').show();
@@ -141,7 +140,6 @@ $(".swiper-slide").hover(
             easing: 'swing',
             complete: function (){}
         })
-        // $('.c2_lunbo_game').css('top','200px')
         $('.c2_lunbo_game').eq(index).stop().animate({
             top:150
         },{
@@ -173,5 +171,56 @@ $(".swiper-slide").hover(
         })
     }
  );
-
-
+//json图片调用
+ $(function(){
+    $.ajax({
+        url:'./zhuye2.json',
+        type:'get',
+        dataType:'json',
+        cache:false,
+        success:function (jsonArr){
+            var results = '';
+            var results2 = '';
+            var results3 = '';
+            var results4 = '';
+            var results5 = '';
+            $.each(jsonArr,function (index,item){
+                results=`
+                    <div class="c2_line1_1s1"></div>
+                    <img src="${item.imgurl}">
+                    <video class="c2_video11" autoplay="autoplay" loop="loop" preload="none" muted="muted">
+                        <source src="${item.vedio}" type="video/webm">
+                    </video>`;
+                results2=`
+                    <div class="c2_line2_1s1"></div>
+                    <img src="${item.imgurl2}">
+                    <video class="c2_video21" autoplay="autoplay" loop="loop" preload="none" muted="muted">
+                        <source src="${item.vedio2}" type="video/webm">
+                    </video>`;
+                results3=`
+                    <div class="c2_line3_1s1"></div>
+                    <img src="${item.imgurl3}">
+                    <video class="c2_video31" autoplay="autoplay" loop="loop" preload="none" muted="muted">
+                        <source src="${item.vedio3}" type="video/webm">
+                    </video>`;
+                results4=`
+                    <div class="c2_line3_1s1"></div>
+                    <img src="${item.imgurl4}">
+                    <video class="c2_video31" autoplay="autoplay" loop="loop" preload="none" muted="muted">
+                        <source src="${item.vedio4}" type="video/webm">
+                    </video>`;
+                results5=`
+                    <div class="c2_line2_1s1"></div>
+                    <img src="${item.imgurl5}">
+                    <video class="c2_video21" autoplay="autoplay" loop="loop" preload="none" muted="muted">
+                        <source src="${item.vedio5}" type="video/webm">
+                    </video>`;
+                $('.c2_line1_1s').eq(index).html(results);
+                $('.c2_line2_1s').eq(index).html(results2);
+                $('.c2_line3_1s').eq(index).html(results3);
+                $('.sb1').eq(index).html(results4);
+                $('.sb2').eq(index).html(results5);
+            });
+        }
+    });
+});
