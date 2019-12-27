a01a0522062f37036ecdbd96f18ace7c4ced5fa7
chenyuebalabala <270210539@qq.com>
2019-12-27 10:40:57 +0800

    55

--- js/zhuye2.js
+++ js/zhuye2.js
@@ -147,7 +147,6 @@ $(".swiper-slide").hover(
             duration: 500,
             easing: 'swing',
             complete: function (){
-                console.log(123)
             }
         })
     },
@@ -162,7 +161,7 @@ $(".swiper-slide").hover(
             complete: function (){}
         })
         $('.c2_lunbo_game').eq(index).stop().animate({
-            top:440
+            top:456
         },{
             queue: true,//加入队列(排队)
             duration: 500,
@@ -197,7 +196,7 @@ $(".swiper-slide").hover(
                     <video class="c2_video21" autoplay="autoplay" loop="loop" preload="none" muted="muted">
                         <source src="${item.vedio2}" type="video/webm">
                     </video>`;
-                results3=`
+                    results3=`
                     <div class="c2_line3_1s1"></div>
                     <img src="${item.imgurl3}">
                     <video class="c2_video31" autoplay="autoplay" loop="loop" preload="none" muted="muted">
@@ -220,7 +219,73 @@ $(".swiper-slide").hover(
                 $('.c2_line3_1s').eq(index).html(results3);
                 $('.sb1').eq(index).html(results4);
                 $('.sb2').eq(index).html(results5);
-            });
+            });            
         }
     });
 });
+
+// $(function(){
+//     $.ajax({
+//         url:'./zhuye2too.json',
+//         type:'get',
+//         dataType:'json',
+//         cache:false,
+//         success:function (jsonArr){
+//             var results = '';                            
+//             $.each(jsonArr,function (index,item){
+//                 results+=`
+//                     <a class="c2_line1_1" href="#">
+//                 <div class="c2_line1_1s">
+//                 <div class="c2_line1_1s1"></div>
+//                     <img src="${item.imgurl}" >
+//                     <video class="c2_video11" autoplay="autoplay" loop="loop" preload="none" muted="muted">
+//                         <source src="${item.vedio}" type="video/webm">
+//                     </video>
+//                 </div>
+//                 <div class="c2_line1_1x">
+//                     <div class="c2_discount">-80%</div>
+//                     <div class="c2_discount_prices">
+//                         <div class="c2_discount_original_price">¥ 120</div>
+//                         <div class="c2_discount_final_price">¥ 24</div>
+//                     </div>
+//                 </div>
+                
+//             </a>`;
+                
+//             });
+            
+//             var sb321=document.createElement('div');
+//             sb321.className = "c2_line1_1y";
+//             var sb123 = `
+//             <div class="c2_1y1">
+//             <h4 class="c2_1h4">Jurassic World Evolution</h4>
+//             <p>发行日期:2018年6月12日</p>
+//             <p>身临其境地沉浸在侏罗纪系列的舞台中，打造您自己的侏罗纪世界。这些通过生物工程打造的恐龙拥有高度智慧，能够对周遭的环境进行思考、感知，并作出反应。而在这个未知的世界里，生命总会找到自己的出路。同时您还要面对商业间谍活动、展品出逃，以及破坏性的热带风暴带来的威胁。</p>
+//             </div>
+//             <div class="c2_1y2">
+//                 <p>总体用户评测</p>
+//                 <p><span>多半好评</span>19,351篇评测</p>
+//             </div>
+//             <ul class="c2_1y3">
+//                 <p>用户标签</p>
+//                 <li>建造</li>
+//                 <li>恐龙</li>
+//                 <li>建造</li>
+//                 <li>恐龙</li>
+//                 <li>建造</li>
+//                 <li>恐龙</li>
+//                 <li>建造</li>
+//             </ul>`;
+//             $(sb321).html(sb123);
+        
+//             $('.c2_line1').html(results);
+//             $('.c2_line1').on('mouseenter','c2_line1_1',function(){
+               
+//                 $(this).append(sb321);
+//             });
+//             $('.c2_line1').on('mouseleave','c2_line1_1',function(){
+//                 $(this).removeChild(sb321);
+//             })
+//         }
+//     });
+// });
